00002de56748bffaced38d877928278235e3274b
RubenSibon <RubenSibon@users.noreply.github.com>
2021-03-28 16:27:22 +0000

    deploy: 3fbf57d5231c9f5da0b9cceca4d3f98e88956e08

--- _nuxt/static/1616948800/blog/hello-world/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616948800",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fhello-world",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ceca32ef"}},__i18n:{langs:{nl:{languages:"Talen",posted:"Geplaatst op",updated:"Aangepast op"},"":{}}}}}(null));
--- _nuxt/static/1616948800/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616948800",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ceca32ef"}},__i18n:{langs:{nl:{languages:"Talen",posted:"Geplaatst op",updated:"Aangepast op"},"":{}}}}}(null,"\u002F"));
