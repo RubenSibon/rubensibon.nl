babdc786f4885dc7c6cf6fd77ba2861d89233a67
Ruben Sibon <r.sibon@amsterdam.nl>
2021-03-28 20:47:12 +0200

    Make nuxt + repo ready for Github pages deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,10 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  router: {
+    base: "/rubensibon.nl/",
+  },
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
