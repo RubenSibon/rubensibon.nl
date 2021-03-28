c6ec6a2b34b3024b212732c8eefecdc1036db32d
Ruben J. Sibon <rubenjs@webricolage.nl>
2021-03-28 18:24:47 +0200

    Add portfolio page

---
+++ pages/portfolio/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <h1>Portfolio</h1>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup () {
+
+  },
+});
+</script>
