514409850cc7861acf2a107fd209ef6585af42eb
RubenSibon <RubenSibon@users.noreply.github.com>
2021-03-30 23:01:37 +0000

    deploy: 930874d62158355a24f49be8a456cd3d2a901d33

---
+++ _nuxt/static/1617145295/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fen\u002Fblog","\u002Fnl\u002Fblog","\u002Fblog","\u002Fportfolio","\u002Fen\u002Fportfolio","\u002Fen","\u002Fnl","\u002Fnl\u002Fportfolio","\u002Fblog\u002Fhello-world","\u002Fen\u002Fblog\u002Fhello-world"]})
