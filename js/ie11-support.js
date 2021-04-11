a12476d7cba679ebeff61feabd70ee5bf4278dd0
RubenSibon <RubenSibon@users.noreply.github.com>
2021-04-11 13:03:18 +0000

    deploy: 33d1603ce1dc42f5648860148505f59edd92ce3d

---
+++ js/ie11-support.js
@@ -0,0 +1,2 @@
+window.MSInputMethodContext && document.documentMode && document.write("<script src=\"https://cdn.jsdelivr.net/gh/nuxodin/ie11CustomProperties@4.1.0/ie11CustomProperties.min.js\"></script>");
+window.MSInputMethodContext && document.documentMode && document.documentElement.setAttribute("is-ie", true);
