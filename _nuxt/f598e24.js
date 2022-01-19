03afb6ff0b2f60eee4e02013f061b28c4c7767fd
RubenSibon <RubenSibon@users.noreply.github.com>
2022-01-19 20:32:03 +0000

    deploy: dccc7b078db33812fa0fc776b1db46eada157fbd

--- _nuxt/f598e24.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{466:function(e,l,t){"use strict";t.r(l);var n=t(66),component=Object(n.a)({},(function(){var e=this,l=e.$createElement;return(e._self._c||l)("div",{staticClass:"flex flex-col w-full max-w-screen-md gap-3 mx-auto ie-gap-vertical"},[e._t("default")],2)}),[],!1,null,null,null);l.default=component.exports}}]);
