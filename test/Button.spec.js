c6b80dd9c8604a409ad7f6947a906ece9a5ce0a9
Ruben J. Sibon <mail@rubensibon.nl>
2023-10-22 20:25:55 +0200

    build: upgrade pkgs; remove unused ones

--- test/Button.spec.js
+++
@@ -1,9 +0,0 @@
-const testUtils = require("@vue/test-utils");
-const Button = import("@/components/Button.vue");
-
-describe("Button", () => {
-  test("is a Vue instance", () => {
-    const wrapper = testUtils.mount(Button);
-    expect(wrapper.vm).toBeTruthy();
-  });
-});
